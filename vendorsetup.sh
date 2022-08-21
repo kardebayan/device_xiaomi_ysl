1df1261ed6a7b9dbb62a8242c50c4870656f6d51
Iris0411 <ashu958895@gmail.com>
2022-08-21 20:43:39 +0530

    ysl: Add vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,23 @@
+echo 'Starting to clone stuffs needed for your device'
+
+echo 'Common Device Tree [1/5]'
+# common-dt
+git clone https://github.com/Iris0411/device_xiaomi_msm8953-common -b 13 device/xiaomi/msm8953-common
+
+echo 'Cloning Prebuilt Kernel tree [2/5]'
+# Vendor Common Tree
+git clone https://github.com/Iris0411/vendor_xiaomi_ysl vendor/xiaomi/ysl
+
+echo 'Cloning Vendor tree [3/5]'
+# Vendor Tree
+git clone https://github.com/Iris0411/vendor_xiaomi_msm8953-common vendor/xiaomi/msm8953-common
+
+echo 'Cloning Kernel tree [4/5]'
+# Kernel Tree
+git clone --depth=1 https://github.com/DhruvChhura/kernel_xiaomi_ysl kernel/xiaomi/ysl
+
+echo 'Cloning Proton Clang [5/5]'
+# Proton Clang
+git clone --depth=1 https://github.com/kdrag0n/proton-clang.git -b master prebuilts/clang/host/linux-x86/clang-proton
+
+echo 'Completed, proceeding to lunch'
